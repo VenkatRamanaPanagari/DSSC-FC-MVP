2d800763d14b83d610537eab971af255544b7d54
VenkatRamanaPanagari <81310237+VenkatRamanaPanagari@users.noreply.github.com>
2022-05-31 08:34:54 -0400

    Create DSSC-FC-MVP.py

---
+++ DSSC-FC-MVP.py
@@ -0,0 +1,53 @@
+# load library
+import streamlit as st
+import numpy as np
+import pandas as pd
+from pandas_datareader import data as pdr
+
+import math
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+import datetime as dt
+import yfinance as yf
+import datetime
+
+# print title of web app
+st.title("Financial Consultation Stock Market Analysis and Prediction")
+st.markdown(">Welcome to the Website")
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+
+# Load data from yahoo finance.
+start=dt.date(2021,1,1)
+end=dt.date(2021,12,31)
+data=pdr.get_data_yahoo("IBM", start, end)
+
+#fill nan vale with next value within columns
+data.fillna(method="ffill",inplace=True)
+
+# Notify the reader that the data was successfully loaded.
+data_load_state.text('Loading data...done!')
+
+
+def convert_df(df):
+     # IMPORTANT: Cache the conversion to prevent computation on every rerun
+     return df.to_csv().encode('utf-8')
+
+csv = convert_df(data)
+
+st.download_button(
+     label="Download data as CSV",
+     data=csv,
+     file_name='data.csv',
+     mime='text/csv',
+ )
+
+
+# create checkbox
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
