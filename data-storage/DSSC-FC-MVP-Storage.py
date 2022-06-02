774049ae67b6fc6ec8380f451ccf0c5cb1c3803e
Hoang Luong <hoangluong1809@gmail.com>
2022-06-02 12:25:41 -0400

    Load data from mongo

--- data-storage/DSSC-FC-MVP-Storage.py
+++ data-storage/DSSC-FC-MVP-Storage.py
@@ -1,7 +1,17 @@
 import pymongo
-client = pymongo.MongoClient("mongodb+srv://wilproject:BteIJ08DLS4Xs0yB@cluster0.umrzxzq.mongodb.net/?retryWrites=true&w=majority")
-db = client.stock
-print(db.list_collection_names())
+import pandas as pd
+import json
+with open('config.json', 'r') as f:
+    config = json.load(f)
+username = config['username']
+password = config['password']
+cluster = config['cluster']
 
-# mongoimport --uri mongodb+srv://wilproject:BteIJ08DLS4Xs0yB@cluster0.umrzxzq.mongodb.net --collection stock --type CSV --headerline --file data.csv
-# mongoimport --uri mongodb+srv://wilproject:BteIJ08DLS4Xs0yB@cluster0.umrzxzq.mongodb.net --collection stock --type CSV --headerline --file data.csv
+# Create connection to MongoDB Atlas
+client = pymongo.MongoClient("mongodb+srv://" + username + ":" + password + " @" + cluster + "/?retryWrites=true&w=majority")
+# Access database
+db = client.stock
+# Access collection
+collection = db.stock_price
+# Achieve the data stored inside of the collection
+data = pd.DataFrame(list(collection.find()))
