7b5e52a44a95fb6079849c355f45c7c647eef7ff
VenkatRamanaPanagari <81310237+VenkatRamanaPanagari@users.noreply.github.com>
2022-06-07 10:39:55 -0400

    Update DSSC-FC-MVP.py

--- DSSC-FC-MVP.py
+++ DSSC-FC-MVP.py
@@ -31,8 +31,7 @@ data_load_state = st.text('Loading data...')
 start=dt.date(2021,1,1)
 end=dt.date(2021,12,31)
 data1=pdr.get_data_yahoo("IBM", start, end)
-data2=pdr.get_data_yahoo("MSFT", start, end)
-data=data1+data2
+
 #fill nan vale with next value within columns
 data.fillna(method="ffill",inplace=True)
 
